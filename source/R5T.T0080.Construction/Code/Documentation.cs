5a7bd0e51a2ca6d7c4dd44aa733cd62a368aee4a
davidcoats <david@getsafetycone.com>
2021-11-09 09:13:28 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0080.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0080.Construction
+{
+    /// <summary>
+    /// Construction project for IConfigurationSourceAction<TConfigurationSource> related types, including interfaces and attributes to mark configuration source action aggregations and aggregation increments..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
